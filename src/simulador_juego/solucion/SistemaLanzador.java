b9d8df966a74970e0edc64e059ced3f1e5b2c14f
Zeldan Campos <zcc1001@alu.ubu.es>
2023-05-30 18:51:27 +0000

    anadir logica sistema lanzador

--- src/simulador_juego/solucion/SistemaLanzador.java
+++ src/simulador_juego/solucion/SistemaLanzador.java
@@ -1,7 +1,22 @@
 package simulador_juego.solucion;
 
 public class SistemaLanzador {
-    public static void main(String[] args){
-        //code
+    public static void main(String[] args) {
+        int numeroTiposEnemigos = 4;
+        int numeroEnemigosPorTipo = 5;
+        int numeroMaximoEnemigosTotales = 10;
+
+        IJuego juego = new Juego(numeroTiposEnemigos, numeroEnemigosPorTipo, numeroMaximoEnemigosTotales);
+
+        for (int tipoEnemigo = 0; tipoEnemigo < numeroTiposEnemigos; tipoEnemigo++) {
+            // Creación de hilos de enemigos
+            ActividadEnemiga enemigos = new ActividadEnemiga(tipoEnemigo, juego);
+            new Thread(enemigos).start();
+
+            // Creación de hilos de aliados
+            ActividadAliada aliados = new ActividadAliada(tipoEnemigo, juego);
+            new Thread(aliados).start();
+        }
+        return;
     }
 }
